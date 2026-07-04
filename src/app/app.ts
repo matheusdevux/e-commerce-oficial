d7934c0e6d9941a071905b2326e41de6b43ccd91
Matheus de sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-04 12:01:12 -0300

    refactor(app): importa Produto e remove RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router'; //Removed because it is not used in this code snippet
+import { Produto } from './components/produto/produto';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
