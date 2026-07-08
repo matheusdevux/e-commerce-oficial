3bf4c462b4e1a19d7c46eae604663d3c6c04fdb0
Matheus de sousa dos Santos <matheusmtsm75@gmail.com>
2026-07-08 14:59:16 -0300

    feat: implementa exibição do fluxo de dados no template

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 export class Produto {
   produto = 'Notebook';
   preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos = [
+    { nome: 'Notebook', preco: 5000 },
+    { nome: 'Smartphone', preco: 3000 },
+    { nome: 'Tablet', preco: 2000 },
+  ];
 }
